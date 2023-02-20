24eabe965eb9e8559947223418d0c4a0442e8169
MinhPhat <phatt@sfu.ca>
2023-02-20 12:24:57 -0800

    update file sensor.py

--- sensor.py
+++ sensor.py
@@ -1,35 +1,36 @@
 #! /usr/bin/python
-import RPI.GPIO as GPIO
+import RPi.GPIO as GPIO
 import time
 
-try:
-    GPIO.setmode(GPIO.BOARD)
+while True:
+    try:
+        GPIO.setmode(GPIO.BOARD)
 
-    PIN_TRIGGER = 7
-    PIN_ECHO = 11
+        PIN_TRIGGER = 7
+        PIN_ECHO = 11
 
-    GPIO.setup(PIN_TRIGGER, GPIO.OUT)
-    GPIO.setup(PIN_ECHO, GPIO.IN)
+        GPIO.setup(PIN_TRIGGER, GPIO.OUT)
+        GPIO.setup(PIN_ECHO, GPIO.IN)
 
-    GPIO.output(PIN_TRIGGER, GPIO.LOW)
-    print("Waiting for sensor to settle")
-    time.sleep(2)
+        GPIO.output(PIN_TRIGGER, GPIO.LOW)
+        print("Waiting for sensor to settle")
+        time.sleep(2)
 
-    print("Calculating distance")
-    GPIO.output(PIN_TRIGGER, GPIO.HIGH)
-    time.sleep(0.00001)
-    GPIO.output(PIN_TRIGGER, GPIO.LOW)
+        print("Calculating distance")
+        GPIO.output(PIN_TRIGGER, GPIO.HIGH)
+        time.sleep(0.00001)
+        GPIO.output(PIN_TRIGGER, GPIO.LOW)
 
-    while GPIO.input(PIN_ECHO) == 0:
-        pulse_start_time = time.time()
-    while GPIO.input(PIN_ECHO) == 1:
-        pulse_end_time = time.time()
+        while GPIO.input(PIN_ECHO) == 0:
+            pulse_start_time = time.time()
+        while GPIO.input(PIN_ECHO) == 1:
+            pulse_end_time = time.time()
 
-    pulse_duration = pulse_end_time - pulse_start_time
-    distance = round(pulse_duration * 34300 * 0.5)
-    print("Distance (cm): ", distance)
+        pulse_duration = pulse_end_time - pulse_start_time
+        distance = round(pulse_duration * 34300 * 0.5)
+        print("Distance (cm): ", distance)
 
-finally:
-    GPIO.cleanup()
+    finally:
+        GPIO.cleanup()
 
  
