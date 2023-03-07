92d48aa6ebc3e5d8b393f5fc9c6d3fce15aead1d
MinhPhat <phatt@sfu.ca>
2023-03-06 20:39:30 -0800

    Add Canvas to GUI for drawing

--- userInterface.py
+++ userInterface.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 from tkinter import *
+import cv2
+from PIL import Image
 
 def reset():
     #do_nothing
@@ -7,8 +9,11 @@ def reset():
 def show_frame(frame):
     frame.tkraise()
 
-x_point = 30
-y_point = 30
+x_ball = 50
+y_ball = 100
+radius = 10
+color = (255, 0, 0)
+thickness = 3
 
 root = tk.Tk()  # create root window
 root.maxsize(880,330)
@@ -21,7 +26,8 @@ root.rowconfigure(0, weight=1)
 root.columnconfigure(0, weight=1)
 frame1 = tk.Frame(root)
 frame2 = tk.Frame(root)
-for frame in (frame1, frame2):
+frame3 = tk.Frame(root)
+for frame in (frame1, frame2, frame3):
     frame.grid(row=0, column=0, sticky='nsew')
 
 #============Frame 1 code===========
@@ -31,7 +37,7 @@ next_image=PhotoImage("exit.png")
 image_label.place(relx=0.5, rely=0.5, anchor=CENTER)
 frame1_btn = tk.Button(frame1, text='Next', image=next_image, command=lambda:show_frame(frame2), bg='cyan2', compound = LEFT)
 frame1_btn.place(x=440, y=200)
-frame1_btn.pack(side=TOP)
+frame1_btn.pack(side=BOTTOM)
 show_frame(frame1)
 
 #============Frame 2 code===========
@@ -44,9 +50,17 @@ right_frame.grid(row=0, column=1, padx=10, pady=10)
 
 image = PhotoImage(file="ironfoot.png")
 original_image = image.subsample(3,3)  # resize image using subsample
+
 Label(left_frame, image=original_image).grid(row=0, column=0, padx=5, pady=5)
 image2 = PhotoImage(file="save2.png")
-Label(right_frame, image=image2).grid(row=0,column=0, padx=5, pady=5)
+
+
+canvas = Canvas(right_frame, bg = "green", height = 300, width = 550)
+canvas.pack(padx = 5, pady = 5)
+canvas.create_image(0, 0, anchor=NW, image=image2)
+canvas.create_oval(x_ball-radius, y_ball-radius, x_ball+radius, y_ball+radius, outline="blue", width=3)
+
+
 tool_bar = Frame(left_frame, width=180, height=185, bg='pink')
 tool_bar.grid(row=2, column=0, padx=5, pady=5)
 textbox = Text(left_frame, width=15, height=5)
@@ -58,5 +72,17 @@ reset_image=PhotoImage(file='reset.png')
 Button(tool_bar, text="Reset", image=reset_image, command=reset, bg='aquamarine').grid(row=1, column=0, padx=5, pady=3, ipadx=10)
 Label(tool_bar, text="PERFORMANCE RATE:",font=("Comic Sans MS", 15, "bold"),bg='pink').grid(row=2, column=0, padx=5, pady=3, ipadx=10)
 textbox.grid(row=3, column=0, padx=5, pady=3, ipadx=10)
+
+#============Frame 3 code===========
+frame3.config(bg="pink")  # specify background color
+left_frame = Frame(frame3, width=200, height=400, bg='pink')
+left_frame.grid(row=0, column=0, padx=10, pady=10)
+
+right_frame = Frame(frame3, width=650, height=400, bg='pink')
+right_frame.grid(row=0, column=1, padx=10, pady=10)
+
+
+Label(left_frame, image=original_image).grid(row=0, column=0, padx=5, pady=5)
+
 
 root.mainloop()
