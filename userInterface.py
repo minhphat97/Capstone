8b1743e9e2ee3380845d167b4b760500fdb2a8a6
MinhPhat <phatt@sfu.ca>
2023-03-08 22:25:15 -0800

    Optimized GUI

--- userInterface.py
+++ userInterface.py
@@ -13,9 +13,9 @@ def show_frame(frame):
 
 x_ball = 50
 y_ball = 100
-radius = 10
+radius = 18
 color = (255, 0, 0)
-thickness = 3
+thickness = 4
 cascade_path = pathlib.Path(cv2.__file__).parent.absolute() / "data/haarcascade_frontalface_default.xml"
 clf = cv2.CascadeClassifier(str(cascade_path))
 root = tk.Tk()  # create root window
@@ -42,15 +42,15 @@ canvas = Canvas(frame1, width=1080, height=566)
 canvas.pack()
 
 canvas.create_image(0, 0, anchor=NW, image=bg_img)
-canvas.create_rectangle(3, 3, 1075, 556, outline = "red", width = 60)
-frame1_btn = tk.Button(frame1, text='PAGE1', command=lambda:show_frame(frame2), bg='cyan2', compound = LEFT)
-frame1_btn.place(x=405, y=530)
-frame1_btn_2 = tk.Button(frame1, text='PAGE2', command=lambda:show_frame(frame3), bg='cyan2', compound = LEFT)
+canvas.create_rectangle(3, 3, 1075, 556, outline = "lightpink2", width = 60)
+frame1_btn = tk.Button(frame1, text='PAGE 1', command=lambda:show_frame(frame2), bg='cyan2', compound = LEFT)
+frame1_btn.place(x=408, y=530)
+frame1_btn_2 = tk.Button(frame1, text='PAGE 2', command=lambda:show_frame(frame3), bg='cyan2', compound = LEFT)
 frame1_btn_2.place(x=590, y=530)
 
 date = dt.datetime.now()
 label = Label(frame1, text=f"{date:%A, %B %d, %Y}", font="Calibri, 20")
-label.place(x=360, y=460)
+label.place(x=340, y=460)
 
 label = Label(frame1, text=strftime('%H:%M:%S %p'), font="Calibri, 20")
 label.place(x=440, y=80)
@@ -68,9 +68,9 @@ image = PhotoImage(file="ironfoot.png")
 original_image = image.subsample(3,3)  # resize image using subsample
 
 Label(left_frame, image=original_image).grid(row=0, column=0, padx=5, pady=5)
-image2 = PhotoImage(file="save2.png")
+image2 = PhotoImage(file="netImage.png")
 
-canvas = Canvas(right_frame, bg = "green", height = 300, width = 550)
+canvas = Canvas(right_frame, bg = "green", height = 532, width = 760)
 canvas.pack(padx = 5, pady = 5)
 canvas.create_image(0, 0, anchor=NW, image=image2)
 canvas.create_oval(x_ball-radius, y_ball-radius, x_ball+radius, y_ball+radius, outline="blue", width=3)
@@ -78,15 +78,18 @@ canvas.create_oval(x_ball-radius, y_ball-radius, x_ball+radius, y_ball+radius, o
 
 tool_bar = Frame(left_frame, width=180, height=185, bg='pink')
 tool_bar.grid(row=2, column=0, padx=5, pady=5)
-textbox = Text(left_frame, width=15, height=5)
+textbox = Text(left_frame, width=20, height=10)
 exit_image=PhotoImage(file='exit.png')
-Button(tool_bar, text="Exit", image=exit_image,command=root.quit, bg='red3').grid(row=0, column=0, padx=5, pady=3, ipadx=10) 
-
+Button(tool_bar, text="Exit", image=exit_image,command=root.quit, bg='red3', width = 50).grid(row=0, column=0, padx=5, pady=3, ipadx=10) 
 reset_image=PhotoImage(file='reset.png')
+Button(tool_bar, text="Reset", image=reset_image, command=reset, bg='aquamarine', width = 50).grid(row=1, column=0, padx=5, pady=3, ipadx=10)
+home_image=PhotoImage(file = "home.png")
+Button(tool_bar, text="Home", image=home_image, command=lambda:show_frame(frame1), bg='dodgerblue1', width = 50).grid(row=2, column=0, padx=5, pady=3, ipadx=10)
+next_image=PhotoImage(file = "next.png")
+Button(tool_bar, text="Next", image=next_image, command=lambda:show_frame(frame3), bg='khaki1', width = 50).grid(row=3, column=0, padx=5, pady=3, ipadx=10)
 
-Button(tool_bar, text="Reset", image=reset_image, command=reset, bg='aquamarine').grid(row=1, column=0, padx=5, pady=3, ipadx=10)
-Label(tool_bar, text="PERFORMANCE RATE:",font=("Comic Sans MS", 15, "bold"),bg='pink').grid(row=2, column=0, padx=5, pady=3, ipadx=10)
-textbox.grid(row=3, column=0, padx=5, pady=3, ipadx=10)
+Label(tool_bar, text="PERFORMANCE RATE:",font=("Comic Sans MS", 15, "bold"),bg='pink').grid(row=4, column=0, padx=5, pady=3, ipadx=10)
+textbox.grid(row=5, column=0, padx=5, pady=3, ipadx=10)
 
 #==============================================Frame 3 code=========================================
 frame3.config(bg="pink")  # specify background color
@@ -94,29 +97,23 @@ left_frame = Frame(frame3, width=200, height=400, bg='pink')
 left_frame.grid(row=0, column=0, padx=10, pady=10)
 
 right_frame = Frame(frame3, width=650, height=400, bg='pink')
-
 right_frame.grid(row=0, column=1, padx=10, pady=10)
 
 Label(left_frame, image=original_image).grid(row=0, column=0, padx=5, pady=5)
 
-# canvas = Canvas(right_frame, bg = "green", height = 300, width = 550)
-# canvas.pack(padx = 5, pady = 5)
-# canvas.create_image(0, 0, anchor=NW, image=image2)
-# canvas.create_oval(x_ball-radius, y_ball-radius, x_ball+radius, y_ball+radius, outline="blue", width=3)
-
 tool_bar = Frame(left_frame, width=180, height=185, bg='pink')
 tool_bar.grid(row=2, column=0, padx=5, pady=5)
-textbox = Text(left_frame, width=15, height=5)
-Button(tool_bar, text="Exit", image=exit_image,command=root.quit, bg='red3').grid(row=0, column=0, padx=5, pady=3, ipadx=10) 
-Button(tool_bar, text="Reset", image=reset_image, command=reset, bg='aquamarine').grid(row=1, column=0, padx=5, pady=3, ipadx=10)
+textbox = Text(left_frame, width=20, height=10)
+Button(tool_bar, text="Exit", image=exit_image,command=root.quit, bg='red3', width = 50).grid(row=0, column=0, padx=5, pady=3, ipadx=10) 
+Button(tool_bar, text="Reset", image=reset_image, command=reset, bg='aquamarine', width = 50).grid(row=1, column=0, padx=5, pady=3, ipadx=10)
 
-Label(tool_bar, text="PERFORMANCE RATE:",font=("Comic Sans MS", 15, "bold"),bg='pink').grid(row=4, column=0, padx=5, pady=3, ipadx=10)
-textbox.grid(row=3, column=0, padx=5, pady=3, ipadx=10)
+Label(tool_bar, text="PERFORMANCE RATE:",font=("Comic Sans MS", 15, "bold"),bg='pink').grid(row=5, column=0, padx=5, pady=3, ipadx=10)
+textbox.grid(row=6, column=0, padx=5, pady=3, ipadx=10)
 
 vid = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-width, height = 50, 20
-vid.set(cv2.CAP_PROP_FRAME_WIDTH, width)
-vid.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+# width, height = 50, 20
+vid.set(cv2.CAP_PROP_FRAME_WIDTH, 700)
+# vid.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
 right_frame.bind('<Escape>', lambda e: right_frame.quit())
 label_widget = Label(right_frame)
 label_widget.pack(padx = 0, pady = 0)
@@ -142,8 +139,18 @@ def open_camera():
     label_widget.configure(image=photo_image)
     label_widget.after(5, open_camera)
 
-Button(tool_bar, text="OpenCamera", command=open_camera, bg='aquamarine').grid(row=2, column=0, padx=5, pady=3, ipadx=10)
-# button1 = Button(right_frame, text="Oen Camera", command=open_camera)
-# button1.pack()
+#Button(tool_bar, text="Open Camera", command=open_camera, bg='aquamarine').grid(row=2, column=0, padx=5, pady=3, ipadx=10)
+webcam_image=PhotoImage(file='webcam.png')
+Button(tool_bar, text="Webcam", image=webcam_image, command=open_camera, bg='aqua', width = 50).grid(row=2, column=0, padx=5, pady=3, ipadx=10)
+Button(tool_bar, text="Home", image=home_image, command=lambda:show_frame(frame1), bg='dodgerblue1', width = 50).grid(row=3, column=0, padx=5, pady=3, ipadx=10)
+back_image=PhotoImage(file='back.png')
+Button(tool_bar, text="Back", image=back_image, command=lambda:show_frame(frame2), bg='khaki1', width = 50).grid(row=4, column=0, padx=5, pady=3, ipadx=10)
+
+
+
+
+
+
+
 
 root.mainloop()
