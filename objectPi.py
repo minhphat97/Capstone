59b384c65e2109d374fb0a268badd296556fc2eb
jg1111 <86036604+JagpreetGrewal@users.noreply.github.com>
2023-04-05 22:29:03 -0700

    Create objectPi.py
    separating object.py into code for laptop and old pi

---
+++ objectPi.py
@@ -0,0 +1,57 @@
+import RPi.GPIO as GPIO
+import socket
+import pickle
+import time
+
+servo_pin = 17
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(servo_pin, GPIO.OUT)
+pwm = GPIO.PWM(servo_pin, 50)
+print("Starting at zero...")
+pwm.start(5)
+
+angle = 90  # set angle Servo
+duty = angle / 27 + 2
+pwm.ChangeDutyCycle(duty)
+
+# Socket setup
+host = ""  # Empty string means the server will accept connections on any available network interface
+port = 12345  # Choose the same unique port number as in the laptop code
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((host, port))
+s.listen(1)  # Listen for incoming connections, with a backlog of 1 connection
+conn, addr = s.accept()  # Accept an incoming connection
+
+while True:
+    try:
+        data = conn.recv(4096)
+        if data:
+            values = pickle.loads(data)
+            x_medium = values["x_medium"]
+            center = values["center"]
+
+            if x_medium < center - 30:
+                angle = angle + 1
+            elif x_medium > center + 30:
+                angle = angle - 1
+
+            duty = angle / 27 + 2
+            pwm.ChangeDutyCycle(duty)
+
+            print("Servo Angle is: ", angle)
+            print("Human Center is: ", x_medium)
+            print("Frame Center is: ", center)
+            print()
+
+    except KeyboardInterrupt:
+        angle = 90
+        duty = angle / 27 + 2
+        pwm.ChangeDutyCycle(duty)
+        break
+
+conn.close()
+s.close()
+pwm.stop()
+GPIO.cleanup()
+       
