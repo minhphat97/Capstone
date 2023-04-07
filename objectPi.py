5ddbab1ccdb26175246852087f3498f0d87d6ff2
Jagpreet Grewal <jsg25@sfu.ca>
2023-04-07 15:27:17 -0700

    changes

--- objectPi.py
+++ objectPi.py
@@ -8,10 +8,10 @@ GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(servo_pin, GPIO.OUT)
 pwm = GPIO.PWM(servo_pin, 50)
-print("Starting at 90...")
+print("Starting at 100...")
 pwm.start(5)
 
-angle = 90  # set angle Servo
+angle = 100  # set angle Servo
 duty = angle / 27 + 2
 pwm.ChangeDutyCycle(duty)
 
@@ -32,8 +32,8 @@ while True:
             center = values["center"]
 
             if x_medium < center - 30:
-                if angle >= 180:
-                    angle = 180
+                if angle >= 140:
+                    angle = 140
                     duty = angle / 27 + 2
                     pwm.ChangeDutyCycle(duty)
                 else:
@@ -41,18 +41,18 @@ while True:
                     duty = angle / 27 + 2
                     pwm.ChangeDutyCycle(duty)
             elif x_medium > center + 30:
-                if angle <= 0:
-                    angle = 0
+                if angle <= 40:
+                    angle = 40
                     duty = angle / 27 + 2
                     pwm.ChangeDutyCycle(duty)
                 else: 
                     angle = angle - 1
                     duty = angle / 27 + 2
                     pwm.ChangeDutyCycle(duty)
-            else:
-                angle = angle
-                duty = angle / 27 + 2
-                pwm.ChangeDutyCycle(duty)
+            # else:
+            #     angle = angle
+            #     duty = angle / 27 + 2
+            #     pwm.ChangeDutyCycle(duty)
 
             print("Servo Angle is: ", angle)
             print("Human Center is: ", x_medium)
