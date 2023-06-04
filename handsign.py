7c84e39e1ee5f411b23edee3ce68c428d57b0c3a
MinhPhat <phatt@sfu.ca>
2023-06-04 01:08:32 -0700

    develop code handsign

--- handsign.py
+++ handsign.py
@@ -9,17 +9,23 @@ while True:
     ret, img = video.read()
     img = cv2.flip(img, 1)
     hand = detector.findHands(img, draw=False)
-    fing = cv2.imread("Put image path with 0 figures up")
+    value = 10
     if hand:
         imlist = hand[0]
         if imlist:
             fingerup = detector.fingersUp(imlist)
             if fingerup == [1, 1, 0, 0, 1]:
+                new_value = value + 5
                 print("It s 1")
+                print("New value: ", new_value)
             elif fingerup == [0, 1, 1, 0, 0]:
+                new_value = value - 5
                 print("It s 2")
+                print("New value: ", new_value)
             elif fingerup == [0, 1, 1, 1, 0]:
+                new_value = value
                 print("It s 3")
+                print("New value: ", new_value)
     
     # fing = cv2.resize(fing, (220, 180))
     # img[50:330, 20:240] = fing
