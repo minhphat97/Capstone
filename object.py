4d2b46b9e6edbea30ba2e84b0004f575cfbf597f
MinhPhat <phatt@sfu.ca>
2023-06-04 20:20:23 -0700

    Added handsign feature to predict human movement

--- object.py
+++ object.py
@@ -2,6 +2,7 @@ import numpy as np
 import cv2
 import RPi.GPIO as GPIO
 import time
+from cvzone.HandTrackingModule import HandDetector
 GPIO.setwarnings(False)
 servo_pin = 17
 GPIO.setmode(GPIO.BCM)
@@ -11,6 +12,7 @@ print("Starting at zero...")
 pwm.start(5) 
 
 cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+detector = HandDetector(maxHands=1, detectionCon=0.8)
 fgbg = cv2.createBackgroundSubtractorMOG2()
 hog = cv2.HOGDescriptor()
 hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
@@ -28,22 +30,43 @@ while(True):
     height, width, _ = frame.shape
     center = int(width/2)
     boxes, weights = hog.detectMultiScale(frame,winStride=(8, 8), padding=(4, 4),scale=1.05)
+    img = cv2.flip(img, 1)
+    hand = detector.findHands(img, draw=False)
     for (x, y, w, h) in boxes:
         pad_w, pad_h = int(0.15 * w), int(0.01 * h)
         cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 255, 0), 2)
         x_medium = int((x + x + w) / 2)
         y_medium = int((y + y + h) / 2)
         break
-    cv2.line(frame, (x_medium, 0), (x_medium, 480), (255, 255, 0), 2)
+    
+    new_x_medium = x_medium
+    if hand:
+        imlist = hand[0]
+        if imlist:
+            fingerup = detector.fingersUp(imlist)
+            if fingerup == [1, 1, 0, 0, 1]:
+                new_x_medium = x_medium + 20
+                print("It s 1")
+                print("New value: ", new_x_medium)
+            elif fingerup == [0, 1, 1, 0, 0]:
+                new_x_medium = x_medium - 20
+                print("It s 2")
+                print("New value: ", new_x_medium)
+            elif fingerup == [0, 1, 1, 1, 0]:
+                new_x_medium = x_medium
+                print("It s 3")
+                print("New value: ", new_x_medium)
+
+    cv2.line(frame, (new_x_medium, 0), (new_x_medium, 480), (255, 255, 0), 2)
     cv2.line(frame, (0, y_medium), (640, y_medium), (255, 255, 0), 2)
     cv2.line(frame, (int(width/2), 0), (int(width/2), 480), (255, 255, 0), 2)
     cv2.line(frame, (0, int(height/2)), (640, int(height/2)), (255, 255, 0), 2)
     cv2.imshow("Human", frame)
-    if x_medium < center - 30:
+    if new_x_medium < center - 30:
         angle = angle + 1
         duty = angle / 27 + 2
         pwm.ChangeDutyCycle(duty)
-    elif x_medium > center + 30:
+    elif new_x_medium > center + 30:
         angle = angle - 1
         duty = angle / 27 + 2
         pwm.ChangeDutyCycle(duty)
@@ -52,7 +75,7 @@ while(True):
         duty = angle / 27 + 2
         pwm.ChangeDutyCycle(duty)
     print("Servo Angle is: ", angle)
-    print("Human Center is: ", x_medium)
+    print("Human Center is: ", new_x_medium)
     print("Frame Center is: ", center)
     print()
     if cv2.waitKey(1) == ord("q"):
