aace6296208ec7b6a19c0a8c3bf10eb1a09de394
Jagpreet Grewal <jaggy1111@gmail.com>
2023-06-17 20:30:38 -0700

    edited setting opencv script

---
+++ setupOpenCV.sh
@@ -0,0 +1,17 @@
+#!/bin/bash	
+# check your memory first
+free -m
+cd ~
+# you need at least a total of 8.5 GB!
+# if not, enlarge your swap space as explained in the guide
+wget https://github.com/Qengineering/Install-OpenCV-Jetson-Nano/raw/main/OpenCV-4-6-0.sh
+sudo chmod 755 ./OpenCV-4-6-0.sh
+./OpenCV-4-6-0.sh
+# once the installation is done...
+rm OpenCV-4-6-0.sh
+# remove the dphys-swapfile now
+#sudo /etc/init.d/dphys-swapfile stop
+#sudo apt-get remove --purge dphys-swapfile
+# just a tip to save an additional 275 MB
+sudo rm -rf ~/opencv
+sudo rm -rf ~/opencv_contrib
