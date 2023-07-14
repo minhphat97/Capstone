2c332def0d4b0b094061a76a37ce858e0ef7e077
Jagpreet Grewal <jaggy1111@gmail.com>
2023-07-14 13:46:58 -0700

    changed servo pin to 1 in testing code

--- servoNano.py
+++ servoNano.py
@@ -2,7 +2,7 @@ from adafruit_servokit import ServoKit
 import time
 import board
 import busio
-servo_pin = 0
+servo_pin = 1
 i2c=busio.I2C(board.SCL_1,board.SDA_1)
 kit = ServoKit(channels=16,i2c=i2c)
 while True:
