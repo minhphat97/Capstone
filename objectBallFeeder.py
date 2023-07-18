29b21608225710e0e5ab5f80887a10006ce5c4e1
MinhPhat <phatt@sfu.ca>
2023-07-17 23:01:56 -0700

    change key

--- objectBallFeeder.py
+++ objectBallFeeder.py
@@ -8,21 +8,16 @@ i2c=board.I2C()
 i2c=busio.I2C(board.SCL_1,board.SDA_1) # this is i2c 0
 kit = ServoKit(channels=16,i2c=i2c)
 
-#angle = 30
 print("STARTING")
 kit.servo[servo_pin].angle = 90
 while True:
-    if keyboard.is_pressed("f"):
-        #ngle = 60
+    if keyboard.is_pressed("4"):
         kit.servo[servo_pin].angle = 120
-        print ("120")
         time.sleep(0.33)
-        #angle = 30
-        #print (angle)
         kit.servo[servo_pin].angle = 90
         time.sleep(3)
         print("DELAY OVER")
-    #print (angle)
+        
 
 
 
