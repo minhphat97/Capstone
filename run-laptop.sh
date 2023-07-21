aa5b194e17b11d4fddbd9cd0c99040fc0b83a72e
Jagpreet Grewal <jsg25@sfu.ca>
2023-07-21 00:40:23 -0700

    preparing to run on .bashrc and systemd

--- run-laptop.sh
+++ run-laptop.sh
@@ -2,3 +2,4 @@
 sudo python3 /home/jg26/Capstone/PID_control-laptop.py &
 sudo python3 /home/jg26/Capstone/objectBallNano-laptop.py # &
 # sudo python3 /jg26/ironfoot/Capstone/PID_control.py #&
+sudo python3 /home/jg26/Capstone/userInterface-laptop.py
