c286af5ef70856ed2ed207d44792e90a1407f4ab
Jagpreet Grewal <jsg25@sfu.ca>
2023-07-20 19:47:05 -0700

    Creating scripts to handle program

---
+++ run-micro.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+sudo python3 /home/ironfoot/Capstone/objectBallFeeder-micro.py &
+# python3 /home/ironfoot/Capstone/objectBallNano.py &
+sudo python3 /home/ironfoot/Capstone/PID_control-micro.py #&
---
+++ start-laptop.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# sudo python3 /home/jg26/Capstone/PID_control-laptop.py &
+# sudo python3 /home/jg26/Capstone/objectBallNano-laptop.py # &
+# sudo python3 /jg26/ironfoot/Capstone/PID_control.py #&
+cleanup() {
+    echo "Cleaning up..."
+    sudo fuser -k 12345/tcp  # This will forcibly kill the process using port 12345
+}
+
+# Trap the EXIT signal and call the cleanup function
+trap cleanup EXIT
+
+cd Capstone
+sudo ./run-laptop.sh
+cd ..
+echo "ALL DONE"
+exit 0
+
+# MOVE TO HOME DIRECTORY OF LAPTOP
+# ENABLE WITH $ sudo chmod +x start-laptop.sh
+# ALSO ENABLE IN THE CAPSTONE DIRECTORY $ sudo chmod +x run-laptop.sh
