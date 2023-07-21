d8b8e24d9b81a55d1ba9adcbc0c25d997e535a9e
Jagpreet Grewal <jaggy1111@gmail.com>
2023-07-21 16:29:17 -0700

    implemented clean up on run.sh script

--- run-micro.sh
+++ run-micro.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+
 sudo python3 /home/ironfoot/Capstone/objectBallFeeder-micro.py &
 # python3 /home/ironfoot/Capstone/objectBallNano.py &
 sudo python3 /home/ironfoot/Capstone/PID_control-micro.py #&
