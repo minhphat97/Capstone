c5ffd2a9695c673efbc2d4b5ea80459a9b602ca8
Jagpreet Grewal <jsg25@sfu.ca>
2023-07-22 13:25:43 -0700

    updated userinterface. using two csv files now

--- userInterface-laptop.py
+++ userInterface-laptop.py
@@ -6,26 +6,20 @@ import pathlib
 import datetime as dt
 from time import strftime
 import csv
-import time
-import subprocess
 
 csv_file_path = '/home/jg26/Capstone/outputtest.csv'
-
+csv_file_path_2 = '/home/jg26/Capstone/outputtestPlayer.csv'
 def reset():
     with open(csv_file_path, 'w') as file:
         file.truncate(0)
-    print("CSV file emptied successfully.")
+    print("CSV file ball emptied successfully.")
+
+    with open(csv_file_path_2, 'w') as file:
+        file.truncate(0)
+    print("CSV file player emptied successfully.")
 
 def show_frame(frame):
     frame.tkraise()
-
-def helloCallBack():
-    subprocess.run("python3 PID_control.py & python3 objectBallFeeder.py.py & python3 objectBallNano.py", shell=True) # previously python objectNano.py & python objectBallNano.py
-    # to open python scripts in separate terminals. Maybe only the PID control should be opened in separate terminal. 
-    # in that case, then remove PID control from the above subprocess, then copy and paste the below line before the subprocess.run line
-    # subprocess.Popen(['gnome-terminal', '--', 'python3', 'PID_control.py'])
-    # subprocess.Popen(['gnome-terminal', '--', 'python3', 'objectBallFeeder.py'])
-    # subprocess.Popen(['gnome-terminal', '--', 'python3', 'objectBallNano.py'])
 
 X = []
 Y = []
@@ -38,16 +32,22 @@ def is_csv_file_empty(csv_file):
         return not any(csv_reader)
 
 if is_csv_file_empty(csv_file_path):
-    print("No data")
-    
+    print("No data ball")
 else:
-    print("Some data")
-    with open('outputtest.csv') as file:
+    print("Some data ball")
+    with open(csv_file_path) as file:
         for row in file:
             X.append(row.split(",")[0])
             Y.append(row.split(",")[1])
-            X_player.append(row.split(",")[2])
-            Y_player.append(row.split(",")[3])
+
+if is_csv_file_empty(csv_file_path_2):
+    print("No data player")
+else:
+    print("Some data player")
+    with open(csv_file_path_2) as file:
+        for row in file:
+            X_player.append(row.split(",")[0])
+            Y_player.append(row.split(",")[1])
 
 radius = 18
 color = (255, 0, 0)
@@ -82,10 +82,8 @@ canvas.create_image(0, 0, anchor=NW, image=bg_img)
 canvas.create_rectangle(3, 3, 1075, 556, outline = "lightpink2", width = 60)
 frame1_btn = tk.Button(frame1, text='PAGE 1', command=lambda:show_frame(frame2), bg='cyan2', compound = LEFT)
 frame1_btn.place(x=408, y=530)
-# frame1_btn_2 = tk.Button(frame1, text='PAGE 2', command=lambda:show_frame(frame3), bg='cyan2', compound = LEFT)
-# frame1_btn_2.place(x=500, y=530)
 frame1_btn_3 = tk.Button(frame1, text='PAGE 2', command=lambda:show_frame(frame4), bg='cyan2', compound = LEFT)
-frame1_btn_3.place(x=628, y=530)
+frame1_btn_3.place(x=590, y=530)
 
 date = dt.datetime.now()
 label = Label(frame1, text=f"{date:%A, %B %d, %Y}", font="Calibri, 20")
@@ -121,8 +119,6 @@ tool_bar.grid(row=2, column=0, padx=5, pady=5)
 
 exit_image=PhotoImage(file='/home/jg26/Capstone/exit.png')
 Button(tool_bar, text="Exit", image=exit_image,command=lambda: [root.quit(), reset()], bg='red3', width = 50).grid(row=0, column=0, padx=5, pady=3, ipadx=10) 
-# reset_image=PhotoImage(file='/home/jg26/Capstone/reset.png')
-# Button(tool_bar, text="Reset", image=reset_image, command=reset, bg='aquamarine', width = 50).grid(row=1, column=0, padx=5, pady=3, ipadx=10)
 home_image=PhotoImage(file = "/home/jg26/Capstone/home.png")
 Button(tool_bar, text="Home", image=home_image, command=lambda:show_frame(frame1), bg='dodgerblue1', width = 50).grid(row=2, column=0, padx=5, pady=3, ipadx=10)
 next_image=PhotoImage(file = "/home/jg26/Capstone/next.png")
@@ -130,60 +126,7 @@ Button(tool_bar, text="Next", image=next_image, command=lambda:show_frame(frame4
 
 Label(tool_bar, text="GOAL POSITION:",font=("Comic Sans MS", 15, "bold"),bg='pink').grid(row=4, column=0, padx=5, pady=3, ipadx=10)
 
-#==============================================Frame 3 code=========================================
-# frame3.config(bg="pink")  # specify background color
-# left_frame = Frame(frame3, width=200, height=400, bg='pink')
-# left_frame.grid(row=0, column=0, padx=10, pady=10)
-
-# right_frame = Frame(frame3, width=650, height=400, bg='pink')
-# right_frame.grid(row=0, column=1, padx=10, pady=10)
-
-# Label(left_frame, image=original_image).grid(row=0, column=0, padx=5, pady=5)
-
-# tool_bar = Frame(left_frame, width=180, height=185, bg='pink')
-# tool_bar.grid(row=2, column=0, padx=5, pady=5)
-# textbox = Text(left_frame, width=20, height=10)
-# Button(tool_bar, text="Exit", image=exit_image,command=root.quit, bg='red3', width = 50).grid(row=0, column=0, padx=5, pady=3, ipadx=10) 
-# Button(tool_bar, text="Reset", image=reset_image, command=reset, bg='aquamarine', width = 50).grid(row=1, column=0, padx=5, pady=3, ipadx=10)
-
-# Label(tool_bar, text="PERFORMANCE RATE:",font=("Comic Sans MS", 15, "bold"),bg='pink').grid(row=5, column=0, padx=5, pady=3, ipadx=10)
-# textbox.grid(row=6, column=0, padx=5, pady=3, ipadx=10)
-
-# vid = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-# # width, height = 50, 20
-# vid.set(cv2.CAP_PROP_FRAME_WIDTH, 700)
-# # vid.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
-# right_frame.bind('<Escape>', lambda e: right_frame.quit())
-# label_widget = Label(right_frame)
-# label_widget.pack(padx = 0, pady = 0)
-
-# def open_camera():
-#     _, frame = vid.read()
-#     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-#     faces = clf.detectMultiScale(
-#         gray,
-#         scaleFactor=1.1,
-#         minNeighbors=5,
-#         minSize=(30, 30),
-#         flags=cv2.CASCADE_SCALE_IMAGE
-#     )
-#     for (x, y, width, height) in faces:
-#         cv2.rectangle(frame, (x, y), (x+width, y+height), (255, 255, 0), 2)
-#         break
-#     opencv_image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
-#     #cv2.imshow("Frame", frame)
-#     captured_image = Image.fromarray(opencv_image)
-#     photo_image = ImageTk.PhotoImage(image=captured_image)
-#     label_widget.photo_image = photo_image
-#     label_widget.configure(image=photo_image)
-#     label_widget.after(5, open_camera)
-
-#Button(tool_bar, text="Open Camera", command=open_camera, bg='aquamarine').grid(row=2, column=0, padx=5, pady=3, ipadx=10)
-# webcam_image=PhotoImage(file='webcam.png')
-# Button(tool_bar, text="Webcam", image=webcam_image, command=open_camera, bg='aqua', width = 50).grid(row=2, column=0, padx=5, pady=3, ipadx=10)
-# Button(tool_bar, text="Next", image=next_image, command=lambda:show_frame(frame4), bg='dodgerblue1', width = 50).grid(row=3, column=0, padx=5, pady=3, ipadx=10)
 back_image=PhotoImage(file='/home/jg26/Capstone/back.png')
-# Button(tool_bar, text="Back", image=back_image, command=lambda:show_frame(frame2), bg='khaki1', width = 50).grid(row=4, column=0, padx=5, pady=3, ipadx=10)
 
 #==============================================Frame 4 code=========================================
 frame4.config(bg="pink")  # specify background color
@@ -208,7 +151,6 @@ tool_bar.grid(row=2, column=0, padx=5, pady=5)
 textbox = Text(left_frame, width=20, height=10)
 
 Button(tool_bar, text="Exit", image=exit_image,command=lambda: [root.quit(), reset()], bg='red3', width = 50).grid(row=0, column=0, padx=5, pady=3, ipadx=10) 
-# Button(tool_bar, text="Reset", image=reset_image, command=reset, bg='aquamarine', width = 50).grid(row=1, column=0, padx=5, pady=3, ipadx=10)
 Button(tool_bar, text="Home", image=home_image, command=lambda:show_frame(frame1), bg='dodgerblue1', width = 50).grid(row=2, column=0, padx=5, pady=3, ipadx=10)
 Button(tool_bar, text="Back", image=back_image, command=lambda:show_frame(frame2), bg='khaki1', width = 50).grid(row=3, column=0, padx=5, pady=3, ipadx=10)
 
