5b8a993e53f117cf9c7226965cca08a3b07c1bea
jg1111 <86036604+JagpreetGrewal@users.noreply.github.com>
2023-07-22 17:53:04 -0700

    Update DS3502.py

--- DS3502.py
+++ DS3502.py
@@ -7,7 +7,7 @@ ds3502 = adafruit_ds3502.DS3502(i2c)
 
 # As this code runs, measure the voltage between ground and the RW (wiper) pin
 # with a multimeter. You should see the voltage change with each print statement.
-for i in range (0, 127, 5):
+for i in range (0, 40, 5):
     ds3502.wiper = i
     print("Wiper value set to ", i)
     sleep(3.0)
