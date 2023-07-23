97270caf5e1a0295d7eaee6d7ccb579314c64369
Jagpreet Grewal <jsg25@sfu.ca>
2023-07-23 16:07:04 -0700

    can do servoNano wirelessly

--- test-laptop.py
+++ test-laptop.py
@@ -27,7 +27,7 @@ print("STARTING CONNECTIONS")
 # List of server IP addresses and corresponding ports
 servers = [
     # ('192.168.1.74', 12345),  # Replace with the actual IP and port of server 1
-    ('127.0.0.1', 12345),  # Replace with the actual IP and port of server 2
+    ('207.23.178.64', 12345),  # Replace with the actual IP and port of server 2
     # ('192.168.1.102', 12345),  # Replace with the actual IP and port of server 3
     # Add more server IP addresses and ports as needed
 ]
@@ -97,62 +97,65 @@ temp = 0
 print("STARTING BALL DETECTOR")
 print("STARTING BALL LAUNCHER")
 print("STARTING BALL FEEDER")
+i = 90 # angle
+direction = 0 # 0 is positive, 1 is negative
+step = 10 # change in angle
 while(True):
-    ret, frame = cap.read()
-    height, width, _ = frame.shape
-    center = int(width/2)
-    boxes, weights = hog.detectMultiScale(frame,winStride=(8, 8), padding=(4, 4),scale=1.05)
+    # ret, frame = cap.read()
+    # height, width, _ = frame.shape
+    # center = int(width/2)
+    # boxes, weights = hog.detectMultiScale(frame,winStride=(8, 8), padding=(4, 4),scale=1.05)
 
-    launch_ball = 0 # this will be 0 every time unless 4 is pressed in the keyboard
+    # launch_ball = 0 # this will be 0 every time unless 4 is pressed in the keyboard
     
-    # if x == 1 and y == 0:
-    if keyboard.is_pressed("1"):
-        flag = 1
-        # print("a is pressed")
-    # elif x == 1 and y == 1:
-    elif keyboard.is_pressed("2"):
-        flag = 2
-        # print("s is pressed")
-    # elif x == 0 and y == 1:
-    elif keyboard.is_pressed("3"):
-        flag = 3
-        # print("d is pressed")
-    if keyboard.is_pressed("4"):
-        print("THREE (3) SECOND DELAY FOR NEXT LAUNCH BALL")
-        launch_ball = 1  
+    # # if x == 1 and y == 0:
+    # if keyboard.is_pressed("1"):
+    #     flag = 1
+    #     # print("a is pressed")
+    # # elif x == 1 and y == 1:
+    # elif keyboard.is_pressed("2"):
+    #     flag = 2
+    #     # print("s is pressed")
+    # # elif x == 0 and y == 1:
+    # elif keyboard.is_pressed("3"):
+    #     flag = 3
+    #     # print("d is pressed")
+    # if keyboard.is_pressed("4"):
+    #     print("THREE (3) SECOND DELAY FOR NEXT LAUNCH BALL")
+    #     launch_ball = 1  
     
-    # ******SERVO ROTATING LAZY SUSAN******
-    for (x, y, w, h) in boxes:
-        rot_angle
-        if flag == 1:
-            x_medium = int((x + x + w) / 2) - 200
-            face_centre_x = x+w/2 - 200
-            y_medium = int((y + y + h) / 2)
-        elif flag == 2:
-            x_medium = int((x + x + w) / 2) 
-            face_centre_x = x+w/2 
-            y_medium = int((y + y + h) / 2)
-        elif flag == 3:
-            x_medium = int((x + x + w) / 2) + 200
-            face_centre_x = x+w/2 + 200
-            y_medium = int((y + y + h) / 2)  
+    # # ******SERVO ROTATING LAZY SUSAN******
+    # for (x, y, w, h) in boxes:
+    #     rot_angle
+    #     if flag == 1:
+    #         x_medium = int((x + x + w) / 2) - 200
+    #         face_centre_x = x+w/2 - 200
+    #         y_medium = int((y + y + h) / 2)
+    #     elif flag == 2:
+    #         x_medium = int((x + x + w) / 2) 
+    #         face_centre_x = x+w/2 
+    #         y_medium = int((y + y + h) / 2)
+    #     elif flag == 3:
+    #         x_medium = int((x + x + w) / 2) + 200
+    #         face_centre_x = x+w/2 + 200
+    #         y_medium = int((y + y + h) / 2)  
 
-        cv2.line(frame, (x_medium, 0), (x_medium, 480), (255, 255, 0), 2)
-        # face_centre_x = x+w/2 
-        error_x = face_centre_x - 320
-        if abs(error_x) > 15:
-            rot_angle = rot_angle - error_x/43
+    #     cv2.line(frame, (x_medium, 0), (x_medium, 480), (255, 255, 0), 2)
+    #     # face_centre_x = x+w/2 
+    #     error_x = face_centre_x - 320
+    #     if abs(error_x) > 15:
+    #         rot_angle = rot_angle - error_x/43
         
-        if rot_angle < 43:
-            rot_angle = 43
-            print("Servo out of range")
+    #     if rot_angle < 43:
+    #         rot_angle = 43
+    #         print("Servo out of range")
         
-        if rot_angle > 137:
-            rot_angle = 137
-            print("Servo out of range")
+    #     if rot_angle > 137:
+    #         rot_angle = 137
+    #         print("Servo out of range")
 
-        # kit.servo[servo_pin].angle = rot_angle  
-        break
+    #     # kit.servo[servo_pin].angle = rot_angle  
+    #     break
 
     # determine second_angle passed to objectBallFeeder.py
     # second_angle = rot_angle # NOT NEEDED, rot_angle already passed in connection
@@ -192,11 +195,9 @@ while(True):
         wiper = 70
         distance = 6.8
     
-    i = 90 # angle
-    direction = 0 # 0 is positive, 1 is negative
-    step = 10 # change in angle
+
     
-    rot_anglee=i
+    rot_angle=i
     result = f'Angle is {i}'
     print(result)
     time.sleep(1)
@@ -235,7 +236,7 @@ while(True):
     # conn2.sendall(data_to_send.encode())
     # conn3.sendall(data_to_send.encode())
 
-    cv2.imshow("Human", frame)
+    # cv2.imshow("Human", frame)
     #if keyboard.is_pressed("5"):
     if cv2.waitKey(1) & keyboard.is_pressed("0"):
         wiper = 0
