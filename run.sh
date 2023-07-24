667afa11c844ecd5f6f490842a6ab67cdb3a57ad
jg1111 <86036604+JagpreetGrewal@users.noreply.github.com>
2023-07-24 16:07:30 -0700

    GUI will run on PID termination

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 sudo python3 /home/ironfoot/Capstone/objectBallFeeder.py &
 sudo python3 /home/ironfoot/Capstone/objectBallNano.py &
-sudo python3 /home/ironfoot/Capstone/PID_control.py #&
+sudo python3 /home/ironfoot/Capstone/PID_control.py 
+# The user interface will run after the above programs are terminated
+sudo python3 /home/ironfoot/Capstone/userInterface.py
